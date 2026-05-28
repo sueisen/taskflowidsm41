b4284294976eb3dd9dcf0e88908813fa5b400663
Sueisen Barraza <117995378+sueisen@users.noreply.github.com>
2026-05-28 09:18:13 -0600

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,16 +1,8 @@
-import json
-import os
-
-FILE = "tasks.json"
-
-def load_tasks():
-    if not os.path.exists(FILE):
-        return []
-
-    with open(FILE, "r") as f:
-        return json.load(f)
-
-
+# Alumno 9
 def save_tasks(tasks):
-    with open(FILE, "w") as f:
-        json.dump(tasks, f, indent=4)
+    pass
+
+
+# Alumno 10
+def load_tasks():
+    return []
