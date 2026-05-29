58ee2a277c01652b08cb81f3b7f82d52cebc51e8
Jose24Piza <135837212+Jose24Piza@users.noreply.github.com>
2026-05-28 23:05:48 -0600

    Agregar mesnajes de error en main.py (#14)
    * Agregar mesnajes de error en main.py
    
    * Se agrego mensaje error en storage.py

--- storage.py
+++ storage.py
@@ -1,8 +1,29 @@
+from utils import show_error
 # Alumno 9
 def save_tasks(tasks):
-    pass
+    try:
+        with open("tasks.txt", "w") as file:
+            for task in tasks:
+                file.write(f"{task['id']},{task['name']},{task['completed']}\n")
+    except Exception as e:
+        show_error("Error al guardar las tareas: " + str(e))
+
+ 
 
 
 # Alumno 10
 def load_tasks():
-    return []
+    try:
+        with open("tasks.txt", "r") as file:
+            tasks = []
+            for line in file:
+                id, name, completed = line.strip().split(",")
+                tasks.append({
+                    "id": id,
+                    "name": name,
+                    "completed": completed == "True"
+                })
+        return tasks
+    except Exception as e:
+        show_error("Error al cargar las tareas: " + str(e))
+        return []
