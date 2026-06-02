0dc80a560c6ebf25e0982ba1b185487c7248f060
Cristal Q <hilaryc.quirinop2010@gmail.com>
2026-06-01 19:12:03 -0600

    feat: persistencia de tareas en JSON

--- storage.py
+++ storage.py
@@ -1,29 +1,23 @@
+import json
 from utils import show_error
 # Alumno 9
 def save_tasks(tasks):
     try:
-        with open("tasks.txt", "w") as file:
-            for task in tasks:
-                file.write(f"{task['id']},{task['name']},{task['completed']}\n")
+        with open("tasks.json", "w", encoding="utf-8") as file:
+            json.dump(tasks, file, indent=4)
     except Exception as e:
         show_error("Error al guardar las tareas: " + str(e))
-
- 
 
 
 # Alumno 10
 def load_tasks():
     try:
-        with open("tasks.txt", "r") as file:
-            tasks = []
-            for line in file:
-                id, name, completed = line.strip().split(",")
-                tasks.append({
-                    "id": id,
-                    "name": name,
-                    "completed": completed == "True"
-                })
-        return tasks
+        with open("tasks.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        return []
+
     except Exception as e:
         show_error("Error al cargar las tareas: " + str(e))
         return []
